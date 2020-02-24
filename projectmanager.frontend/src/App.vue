19db5d45a76f5576c9f3f14dc31ad1f09c225ffc
Leonardo Nogueira da Silva <lnogueiradasilva628@gmail.com>
2020-02-24 09:22:03 -0300

    app configurado

--- projectmanager.frontend/src/App.vue
+++ projectmanager.frontend/src/App.vue
@@ -1,28 +1,109 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="principalApp" class="page-container md-layout-column">
+		<md-toolbar class="md-primary">
+			
+			<md-button  class="md-icon-button" @click="showNavigation = !showNavigation">
+				<md-icon>menu</md-icon>
+			</md-button>
+
+			<span class="md-title">Project Manager</span>
+			
+			<md-button  class="md-icon-button" to="/">
+				<md-icon id="iconHome">home</md-icon>
+			</md-button>
+
+		</md-toolbar>
+
+		<!-- Menu lateral esquerda (opcoes) -->
+		<md-drawer
+			:md-active.sync="showNavigation" 
+			md-swipeable 
+			class="buttons-menu-options">
+
+			<md-toolbar class="md-transparent" md-elevation="0">
+				<span class="md-title">Menu</span>
+			</md-toolbar>
+
+			<md-list id="routesMenu">
+				
+				<md-list-item>
+				
+					<md-button 
+						to="/"
+						@click.native="closeLeftMenu">
+				
+						<md-icon>schedule</md-icon>
+				
+						<span >Projetos</span>
+					</md-button>
+				</md-list-item>
+			</md-list>
+		</md-drawer>
+
+		<md-content>
+			<router-view></router-view>        
+		</md-content>
+	</div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
 
+<script>
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+	
+	name: 'app',
+	
+	data: () => ({
+		showNavigation: false,
+		showSidepanel: false,
+		departament:null,
+	}),
+	
+	methods:{
+		
+		closeLeftMenu(){
+			this.showNavigation = !this.showNavigation
+		},
+		
+		sync(){
+			this.$http.get('sync')
+			this.closeLeftMenu()
+		},
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+
+	.buttons-menu-options {
+
+		a, button {
+			width: inherit;
+		}
+
+		.md-icon{
+			margin-right: 10px !important;
+		}
+
+		.md-button .md-ripple {
+			justify-content: left;
+		}
+	}
+
+	.dc-datepicker > .md-icon.md-theme-default.md-icon-image svg{
+		fill: var(--md-theme-default-primary) !important;
+	}
 </style>
+
+<style lang="scss" scoped>
+
+	.md-content {
+		padding: 20px;
+	}
+
+	.text-notify{
+		text-align: center;
+	}
+
+</style>
+
+
